6a5671e45800c6904fdf695597050990835287c4
kielson.silva <kielson.silva@vdi-unijava-004.intranet.ciss>
2015-04-14 16:25:33 -0300

    COrreçoes gravar

--- Odontologico/src/br/com/uniciss/odontologico/funcionario/Secretario.java
+++ Odontologico/src/br/com/uniciss/odontologico/funcionario/Secretario.java
@@ -1,22 +1,16 @@
 package br.com.uniciss.odontologico.funcionario;
 
-import java.util.Scanner;
-
 import br.com.uniciss.odontologico.BD.Gravar;
 
 public class Secretario extends Funcionario {
-	Scanner entrada = new Scanner(System.in);
-
+	
 	public void cadastrarSecretario() {
-		super.cadastraPessoa();
+		super.cadastraFuncionario();
 		
 		Gravar g = new Gravar();
-		g.grava("secretarios.txt", toString());
-		g.grava("Usuarios.txt", toString2());
-		teclado.close();
+		g.grava("br/com/uniciss/odontologico/documentos/secretarios.txt", toString());
+		g.grava("br/com/uniciss/odontologico/documentos/users.txt", toString2());
 		
-	
-
 	}
 
 	public void listarPaciente() {
