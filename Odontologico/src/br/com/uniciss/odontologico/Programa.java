661bc3c4b7532ef88c68df6786ba675a4dc0d839
LuizGustavo <LuizGustavo@LuizGustavo>
2015-04-14 15:03:34 -0300

    att pessoa

--- Odontologico/src/br/com/uniciss/odontologico/Programa.java
+++ Odontologico/src/br/com/uniciss/odontologico/Programa.java
@@ -1,9 +1,15 @@
 package br.com.uniciss.odontologico;
 
+import br.com.uniciss.odontologico.cliente.Cliente;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Sistema de atendimento Odontologico");
+		
+		Cliente cliente = new Cliente();
+		
+		cliente.cadastraCliente();
 
 	}
 
--- Odontologico/src/br/com/uniciss/odontologico/cliente/Pessoa.java
+++ Odontologico/src/br/com/uniciss/odontologico/cliente/Pessoa.java
@@ -1,11 +1,51 @@
 package br.com.uniciss.odontologico.cliente;
 
-
 public abstract class Pessoa {
-	public String nome;
-	public String rg;
-	public String cpf;
-	public String endereco;
-	public int codigo;
-	public boolean status;
+	protected String nome;
+	protected String rg;
+	protected String cpf;
+	protected String endereco;
+	protected int codigo;
+	protected boolean status;
+	
+	//Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	
+	
 }
