41a4a5316410146faf65eb1fb6127e8ac87e56cf
gabriel.paula <gabriel.paula@vdi-unijava-002.intranet.ciss>
2015-04-14 16:17:14 -0300

    Coisas

--- Odontologico/src/br/com/uniciss/odontologico/funcionario/Secretario.java
+++ Odontologico/src/br/com/uniciss/odontologico/funcionario/Secretario.java
@@ -1,20 +1,34 @@
 package br.com.uniciss.odontologico.funcionario;
 
+import java.util.Scanner;
 
-public class Secretario {
-	public void cadastrarPaciente() {
+import br.com.uniciss.odontologico.BD.Gravar;
+
+public class Secretario extends Funcionario {
+	Scanner entrada = new Scanner(System.in);
+
+	public void cadastrarSecretario() {
+		super.cadastraPessoa();
+		cadastraFuncionario();
+		
+		Gravar g = new Gravar();
+		g.grava("/Secretarios.txt", toString());
+		g.grava("Usuarios.txt", toString2());
+		teclado.close();
+		
 	
+
 	}
-	
+
 	public void listarPaciente() {
-	
+
 	}
-	
+
 	public void editarPaciente() {
-	
+
 	}
-	
+
 	public void alterarStatusPaciente() {
-	
+
 	}
 }
