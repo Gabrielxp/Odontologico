7a9b5b65f4d171ebd5fc60d15fb71726f84e21c2
Marco <Marco@172.0.0.154>
2015-04-14 14:57:29 -0300

    Getters E Setters do Funcionario

--- Odontologico/src/br/com/uniciss/odontologico/funcionario/Funcionario.java
+++ Odontologico/src/br/com/uniciss/odontologico/funcionario/Funcionario.java
@@ -3,11 +3,33 @@ package br.com.uniciss.odontologico.funcionario;
 import java.util.Date;
 
 
+abstract public class Funcionario {
+	protected Date HoraDeEntrada;
+	protected Date HoraDeSaida;
+	protected String login;
+	protected String senha;
+	protected Date getHoraDeEntrada() {
+		return HoraDeEntrada;
+	}
+	public void setHoraDeEntrada(Date horaDeEntrada) {
+		HoraDeEntrada = horaDeEntrada;
+	}
+	public Date getHoraDeSaida() {
+		return HoraDeSaida;
+	}
+	public void setHoraDeSaida(Date horaDeSaida) {
+		HoraDeSaida = horaDeSaida;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public void cadastraFuncionario(){
+		
+	}
 
-
-
-public class Funcionario {
-	public Date HoraDeEntrada;
-	public Date HoraDeSaida;
-	public String senha;
+	
 }
