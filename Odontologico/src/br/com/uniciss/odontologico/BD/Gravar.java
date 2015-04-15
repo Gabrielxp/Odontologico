80b32d8dec5be36be1287f741db8e4dcc2db83d7
Marco <Marco@172.16.105.36>
2015-04-15 14:02:11 -0300

    Inicio Login

--- Odontologico/src/br/com/uniciss/odontologico/BD/Gravar.java
+++ Odontologico/src/br/com/uniciss/odontologico/BD/Gravar.java
@@ -1,9 +1,15 @@
 package br.com.uniciss.odontologico.BD;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
+
+import br.com.uniciss.odontologico.funcionario.Funcionario;
+import br.com.uniciss.odontologico.funcionario.Secretario;
 
 public class Gravar {
 	public void grava(String aonde,String oque){
@@ -19,5 +25,30 @@ public class Gravar {
 		}catch (IOException e){
 			
 		} 
+	}
+		public void leituraUsuario(Map<String, Funcionario> listaUsuario){
+			try {
+				FileReader arq = new FileReader("users.txt");
+				BufferedReader lerArq = new BufferedReader(arq); 
+				String linha = lerArq.readLine();  
+				
+				
+				while (linha != null) {
+					String palavras[]=linha.split(",");
+					Funcionario f = new Secretario();
+					
+					f.setLogin(palavras[1]);
+					f.setSenha(palavras[2]);
+					f.setTipo(palavras[3]);
+					
+					listaUsuario.put(f.getLogin(), f);
+					linha = lerArq.readLine();
+				} 
+				arq.close(); 
+			} 
+			catch (IOException e) { 
+				System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+			}	
+
 	}
 }
--- Odontologico/src/br/com/uniciss/odontologico/Programa.java
+++ Odontologico/src/br/com/uniciss/odontologico/Programa.java
@@ -1,15 +1,14 @@
 package br.com.uniciss.odontologico;
 
-import br.com.uniciss.odontologico.funcionario.Dentista;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		System.out.println("Sistema de atendimento Odontologico");
-
-		Dentista dentista = new Dentista();
-
-		dentista.cadastrarDentista();
+		Menu m = new Menu();
+		
+		m.login();
+		
 		
 		
 	}
