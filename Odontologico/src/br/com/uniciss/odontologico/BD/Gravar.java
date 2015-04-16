27853523d9b54c8bb3767797b3a8e25229f17178
gabriel.paula <gabriel.paula@vdi-unijava-002.intranet.ciss>
2015-04-16 17:00:48 -0300

    Admin Quase

--- Odontologico/src/br/com/uniciss/odontologico/BD/Gravar.java
+++ Odontologico/src/br/com/uniciss/odontologico/BD/Gravar.java
@@ -13,49 +13,60 @@ import br.com.uniciss.odontologico.funcionario.Funcionario;
 import br.com.uniciss.odontologico.funcionario.Secretario;
 
 public class Gravar {
-	public void grava(String aonde,String oque) throws IOException{
-		File ficheiro = new File(aonde); 
-		try { 
-			BufferedWriter bw = new BufferedWriter(new FileWriter(ficheiro, true));  
-			if(ficheiro.canWrite()){ 
-				bw.write(oque); 
-				bw.newLine(); 
-				bw.flush(); 
-				bw.close(); 
-			} 
-		}catch (java.io.FileNotFoundException e){
+	public void grava(String aonde, String oque) throws IOException {
+		File ficheiro = new File(aonde);
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(ficheiro,
+					true));
+			if (ficheiro.canWrite()) {
+				bw.write(oque);
+				bw.newLine();
+				bw.flush();
+				bw.close();
+			}
+		} catch (java.io.FileNotFoundException e) {
 			System.out.println("Erro");
-		} 
+		}
 	}
-		public void leituraUsuario(Map<String, Funcionario> listaUsuario){
-			try {
-				FileReader arq = new FileReader("documentos/users.txt");
-				BufferedReader lerArq = new BufferedReader(arq); 
-				String linha = lerArq.readLine();  
-				
-				
-				while (linha != null) {
-					String palavras[]=linha.split(",");
-					Funcionario f = new Secretario();
-					
-					f.setLogin(palavras[1]);
-					f.setSenha(palavras[2]);
-					f.setTipo(palavras[3]);
-					
-					listaUsuario.put(f.getLogin(), f);
-					linha = lerArq.readLine();
-				} 
-				arq.close(); 
-			} 
-			catch (IOException e) { 
-				System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
-			}	
+
+	public void leituraUsuario(Map<String, Funcionario> listaUsuario) {
+		try {
+			FileReader arq = new FileReader("documentos/users.txt");
+			BufferedReader lerArq = new BufferedReader(arq);
+			String linha = lerArq.readLine();
+
+			while (linha != null) {
+				String palavras[] = linha.split(",");
+				Funcionario f = new Secretario();
+
+				f.setLogin(palavras[1]);
+				f.setSenha(palavras[2]);
+				f.setTipo(palavras[3]);
+
+				listaUsuario.put(f.getLogin(), f);
+				linha = lerArq.readLine();
+			}
+			arq.close();
+		} catch (IOException e) {
+			System.err.printf("Erro na abertura do arquivo: %s.\n",
+					e.getMessage());
+		}
 
 	}
-		public void editar(String aonde) throws IOException{
-			File arq = new File(aonde+".txt");
-			arq.delete();
-			File novo = new File(aonde+".txt");
-			
+
+	public void editar(String aonde) throws IOException {
+		File ficheiro = new File(aonde);
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(ficheiro));
+			if (ficheiro.canWrite()) {
+				bw.write("");
+
+				bw.flush();
+				bw.close();
+			}
+		} catch (java.io.FileNotFoundException e) {
+			System.out.println("Erro");
 		}
+
+	}
 }
--- Odontologico/src/br/com/uniciss/odontologico/admin/Admin.java
+++ Odontologico/src/br/com/uniciss/odontologico/admin/Admin.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import br.com.uniciss.odontologico.Menus;
+import br.com.uniciss.odontologico.BD.Gravar;
 import br.com.uniciss.odontologico.BD.LeituraDeDados;
 import br.com.uniciss.odontologico.funcionario.Dentista;
 import br.com.uniciss.odontologico.funcionario.Secretario;
@@ -15,23 +17,56 @@ import br.com.uniciss.odontologico.funcionario.Secretario;
 public class Admin {
 	Scanner entrada = new Scanner(System.in);
 	LeituraDeDados leia = new LeituraDeDados();
-	public void editarSecretario() throws FileNotFoundException, IOException{
-		System.out.println("Determine o nome ");
-		String nome = entrada.nextLine();
-		List<Secretario>listaSecretario = new ArrayList<Secretario>();
-		Map<Integer, Secretario>mapaSecretario = new HashMap<Integer, Secretario>();
+
+	public void editarSecretario() throws FileNotFoundException, IOException {
+
+		String nome;
+		List<Secretario> listaSecretario = new ArrayList<Secretario>();
+		Map<Integer, Secretario> mapaSecretario = new HashMap<Integer, Secretario>();
 		leia.leituraSecretario(listaSecretario, mapaSecretario);
-		
-		System.out.println("TESTEEE");
-		for(Secretario s:listaSecretario){
-			if (nome.equals(s.getNome())){
-				s.cadastraFuncionario();
+		Menus m = new Menus();
+		System.out.println("Determine o nome da pessoa a ser editado: ");
+		nome = entrada.nextLine();
+		boolean continua = false;
+
+		for (Secretario s : listaSecretario) {
+
+			if (nome.equals(s.getNome())) {
+
+				s.cadastro();
+				continua = true;
+
 			}
-		} 
+			Gravar g = new Gravar();
+			g.editar("documentos/secretarios.txt");
+			for (Secretario f : listaSecretario) {
+				g.grava("documentos/secretarios.txt", f.toString());
+			}
+
+		}
+
+		if (continua == false) {
+
+			int escolha;
+
+			System.out.println("Determinaste um nome invalido"
+					+ "\n Deseja voltar ao menu? 1 - sim, 2 - não");
+			escolha = entrada.nextInt();
+			entrada.nextLine();
+			if (escolha == 1) {
+				m.menuAdmin();
+			} else if (escolha != 1) {
+				editarSecretario();
+			}
+		}
+		if (continua == true) {
+			System.out.println("\n Edição realizada com sucesso!\n");
+			m.menuAdmin();
+		}
 	}
-	public void editarDentista(){
-		
+
+	public void editarDentista() {
+
 	}
-	
-	
+
 }
