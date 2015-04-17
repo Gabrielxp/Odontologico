33dc2238bca7787197c5ee0e18d61dfb4743406b
alisson.carenhato <alisson.carenhato@vdi-unijava-009.intranet.ciss>
2015-04-17 01:55:17 -0300

    Inativar Secretario

--- Odontologico/src/br/com/uniciss/odontologico/funcionario/Secretario.java
+++ Odontologico/src/br/com/uniciss/odontologico/funcionario/Secretario.java
@@ -14,12 +14,12 @@ import br.com.uniciss.odontologico.BD.LeituraDeDados;
 import br.com.uniciss.odontologico.cliente.Cliente;
 
 public class Secretario extends Funcionario {
-	
+
 	Scanner teclado = new Scanner(System.in);
-	
+
 	public void cadastrarSecretario() throws FileNotFoundException, IOException {
 		super.cadastraFuncionario();
-		
+
 		tipo = "secretario";  
 		setStatus(true);
 		Gravar g = new Gravar();
@@ -27,21 +27,21 @@ public class Secretario extends Funcionario {
 		g.grava("documentos/users.txt", toString2());
 		return;
 	}
-	
+
 	public void consultarPaciente(){
 		List<Cliente> listaPaciente = new ArrayList<Cliente>();
-		
+
 		LeituraDeDados leitura = new LeituraDeDados();
 		leitura.leituraPacientes(listaPaciente);
-		
+
 		System.out.println("Informe o nome do Paciente");
 		String nome = teclado.nextLine();
 		nome = nome.replace(" ", "");
 		nome = nome.toLowerCase();
-		
+
 		for(Cliente paciente : listaPaciente){
 			String nomeDaVez = paciente.getNome().replace(" ", "");
-			
+
 			if(nomeDaVez.toLowerCase().equals(nome)){
 				System.out.println("------- Paciente "+paciente.getNome()+"------");
 				if(!paciente.isStatus()){
@@ -55,37 +55,37 @@ public class Secretario extends Funcionario {
 				System.out.println("---------------------");
 			}
 		}
-		
+
 	}
 
 	public void consultarSecretario() {
 
 		int opc;
 		LeituraDeDados leitura = new LeituraDeDados();
-		
+
 		List<Secretario> listaSecretario = new ArrayList<Secretario>();
 		Map<Integer, Secretario> mapaSecretario = new HashMap<Integer, Secretario>();
-		
+
 		leitura.leituraSecretario(listaSecretario, mapaSecretario);
-		
+
 		System.out.println("    SELECIONE UMA OPÇÃO ");  
 		System.out.println("1- Consultar Secretario por Nome");
 		System.out.println("2- Consultar Secretario por ID");
-		
+
 		teclado = new Scanner(System.in);
 		opc = teclado.nextInt();
 		teclado.nextLine();
-		
+
 		switch (opc) {
 		case 1:
 			System.out.println("Informe o nome do Secretario");
 			String nome = teclado.nextLine();
 			nome = nome.replace(" ", "");
 			nome = nome.toLowerCase();
-			
+
 			for(Secretario secretario: listaSecretario){
 				String nomeDaVez = secretario.getNome().replace(" ", "");
-				
+
 				if(nomeDaVez.toLowerCase().equals(nome)){
 					System.out.println("------- Secretario "+secretario.getNome()+"------");
 					if(!secretario.isStatus()){
@@ -100,15 +100,15 @@ public class Secretario extends Funcionario {
 					System.out.println("Hora Saida:"+secretario.getHoraDeSaida());
 					System.out.println("---------------------");
 				}
-				
+
 			}
 			break;
 		case 2:
 			Secretario secretario = new Secretario();
-			
+
 			System.out.println("Informe seu ID");
 			int cro = teclado.nextInt();
-			
+
 			try{
 				secretario = listaSecretario.get(cro);
 				System.out.println("------- Secretario "+secretario.getNome()+"------");
@@ -126,11 +126,52 @@ public class Secretario extends Funcionario {
 			}catch(NullPointerException e){
 				System.out.println("Secretario não encontrado");
 			}
-			
+
 			break;
 		}
-	
+
 	}
+	public String toStringSecretario(){
+		return "Secretario"+","+getCodigo()+","+getNome()+","+getRg()+","+getCpf()+","+getEndereco()+","+getDataDeNascimento()+","+isStatus()+","+getHoraDeEntrada()+","+getHoraDeSaida();
+	}
+
+
+	public void inativarSecretario() throws IOException{
+		LeituraDeDados leitura = new LeituraDeDados();
+
+		List<Secretario>listaSecretario = new ArrayList<Secretario>();
+		Map<Integer, Secretario>mapaSecretario = new HashMap<Integer, Secretario>();
+
+		leitura.leituraSecretario(listaSecretario, mapaSecretario);;
+		teclado = new Scanner(System.in);
+
+		System.out.println("Informe o codigo do Secretario");
+		int codSecretario = teclado.nextInt();
+
+		boolean existe = false;
+
+		for(Secretario s : listaSecretario){
+			if(s.getCodigo() == codSecretario){
+
+				existe = true;
+				if(!s.isStatus()){
+					System.out.println("Este Secretario ja foi inativado");
+				}else{
+					s.setStatus(false);
+
+					Gravar g = new Gravar();
+					g.editar("documentos/secretarios.txt");
+
+					for (Secretario ss : listaSecretario) {
+						g.grava("documentos/secretarios.txt", ss.toStringSecretario());
+					}
+
+				}
+			}
+		}	
+	}
+	
+	
 
 	public void editarPaciente() {
 
