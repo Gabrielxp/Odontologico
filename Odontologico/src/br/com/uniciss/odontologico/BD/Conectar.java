b33eff474cf57c88c470ff58624358588f8b1b3e
gabriel.paula <gabriel.paula@vdi-unijava-002.intranet.ciss>
2015-05-07 15:21:23 -0300

    sCRIPTS E cONECTAR

--- Odontologico/src/br/com/uniciss/odontologico/BD/Conectar.java
+++ Odontologico/src/br/com/uniciss/odontologico/BD/Conectar.java
@@ -7,57 +7,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Conectar {
-	public String select(String identificador){
-	
-		
-		
-		
-		
-		return identificador;
-		}
-	
-	public void intert(){
-		
-		
-	}
+	static Connection conexao;
 
-	public static void main(String[] args) throws SQLException,
-			ClassNotFoundException {
-
+	public Connection conectar() throws ClassNotFoundException, SQLException {
+		
 		Class.forName("org.postgresql.Driver");
-		Connection conexao = DriverManager.getConnection(
+		conexao = DriverManager.getConnection(
 				"jdbc:postgresql://localhost:5432/Projeto_Odontologico",
 				"postgres", "gsp020596");
 		System.out.println("Conectado!");
-		
-		
-	
-		
-		try {
-			String sql = "INSERT INTO pessoa (id_pessoa, nome, cpf, endereco, data_nascimento) "
-					+ "VALUES (7,'Corleone','12345678911',1,'1990-02-04')";
-			PreparedStatement ps = conexao.prepareStatement(sql);
-			ps.execute();
-		} catch (Exception e) {
-			System.out.println("ID JÁ CADASTRADO!");
+		return conexao;
 
-		}
-
-		
-		String sql1 = "SELECT id_pessoa, nome, cpf, endereco, data_nascimento FROM pessoa";
-		PreparedStatement ps1 = conexao.prepareStatement(sql1);
-		ps1.execute();
-		ResultSet rs = ps1.executeQuery();
-		while(rs.next()){
-			int idPessoa = rs.getInt("id_pessoa");
-			String nome = rs.getString("nome");
-			String cpf = rs.getString("cpf");
-			int endereco = rs.getInt("endereco");
-			System.out.println("ID: "+idPessoa+" Nome:"+nome+" cpf: "+cpf+"\n endereco:"+endereco);
-		}
-	
-
-		conexao.close();
 	}
 
+	public static void desconectar() throws SQLException {
+
+		conexao.close();
+
+	}
+
+
 }
+
---
+++ Odontologico/src/br/com/uniciss/odontologico/BD/Scripts.java
@@ -0,0 +1,41 @@
+package br.com.uniciss.odontologico.BD;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Scripts {
+
+	private Connection conexao;
+
+	public void insert(String sql) throws SQLException {
+		try {
+			Connection conexao = null;
+			PreparedStatement ps = conexao.prepareStatement(sql);
+			ps.execute();
+		} catch (Exception e) {
+			System.out.println("ID JÁ CADASTRADO!");
+		}
+	}
+
+	public void select() throws ClassNotFoundException, SQLException {
+
+		conexao = new Conectar().conectar();
+		String sql = "SELECT id_pessoa, nome, cpf, endereco, data_nascimento FROM pessoa";
+		PreparedStatement ps = conexao.prepareStatement(sql);
+
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			int idPessoa = rs.getInt("id_pessoa");
+			String nome = rs.getString("nome");
+			String cpf = rs.getString("cpf");
+			int endereco = rs.getInt("endereco");
+
+			System.out.println("ID: " + idPessoa + " Nome:" + nome + " cpf: "
+					+ cpf + "\n endereco:" + endereco);
+		}  
+
+	}
+
+}
